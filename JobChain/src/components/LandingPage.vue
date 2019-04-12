271f10381aeb94d5687baff760c8d8d7e6092c72
santai <you@example.com>
2019-04-12 13:36:27 +0800

    Integrat policy component

--- JobChain/src/components/LandingPage.vue
+++ JobChain/src/components/LandingPage.vue
@@ -1,50 +1,153 @@
 <template>
-  <div>
-    <div class='navbar'>
-      <span class='engtext font_size_nav'>Jobchain </span><span class='chitext font_size_nav'>跨界人才交流計畫</span>
+  <div id='landing-page'>
+    <div id='navbar-block'>
+      <span class='text-english g-font-size'>Jobchain </span><span class='text-chinese g-font-size'>跨界人才交流計畫</span>
     </div>
+
     <div>
       <div class='margin_top_word1'>
-        <span class='engtext font_size_top1'>建立我的專業人脈網</span>
+        <span class='text-english font_size_top1'>建立我的專業人脈網</span>
       </div>
       <div class='margin_top_word2'>
-        <span class='chitext font_size_nav'>發揮所長，讓專業</span><span class='engtext font_size_nav'>Chain</span><span class='chitext font_size_nav'>在一起</span>
+        <p class='text-chinese g-font-size'>
+          發揮所長，讓專業<span class='text-english g-font-size'>Chain</span>在一起
+        </p>
       </div>
+    </div>
+
+    <div>
+      <span class='top_button text-chinese font_size_top_button color_top_button'>開始交流</span>
+    </div>
+
+    <div id='top_svg_1'>
+      <img src='@/assets/svg/pic_landing_page_1.svg' />
+    </div>
+
+    <hr class='left_hr' />
+
+    <div class=''>
+      <p class='text-chinese'>
+        每週人才推薦
+      </p>
+      <p class='text-chinese'>
+        <span class='text-english'>Chain-up</span>將於每週推薦一位跨業人才給您做交流， 讓您輕鬆建立自己的專業人脈網。
+      </p>
+    </div>
+
+    <div id='top_svg_2'>
+      <img src='@/assets/svg/pic_landing_page_2.svg' />
+    </div>
+
+    <hr class='right_hr' />
+
+    <div class=''>
+      <p class='text-chinese'>
+        與跨業人才合作
+      </p>
+      <p class='text-chinese'>
+        <span class='text-english'>Chain-up</span><b>期望透過平台的交流功能，讓您找到能一起完成</b>
+        <span class='text-english'>side project</span>或是一起經營事業的合作夥伴。
+      </p>
+    </div>
+
+    <div id='top_svg_3'>
+      <img src='@/assets/svg/pic_landing_page_3.svg' />
+    </div>
+
+    <hr class='left_hr' />
+
+    <div class=''>
+      <p class='text-chinese'>
+        跨業人才交流會
+      </p>
+      <p class='text-chinese'>
+        <span class='text-english'>Chain-up</span>將於每月舉辦一場小型的人才交流會，您可以在交 流會中分享自己的專業。
+      </p>
+    </div>
+
+    <div id='top_svg_4'>
+      <img src='@/assets/svg/pic_landing_page_4.svg' />
+    </div>
+
+    <hr class='right_hr' />
+
+    <div>
+      <span class='top_button text-chinese font_size_top_button color_top_button'>開始交流</span>
+    </div>
+
+    <div>
+      <span>訂閱我們</span>
+      <span>每個月收到我們精選的活動成果及最新通知</span>
+    </div>
+
+    <div>
+      <input type='text'>
+    </div>
+
+    <div>
+      <span class='top_button text-chinese font_size_top_button color_top_button'>^</span>
+    </div>
+
+    <div id='footer'>
+
       <div>
-        <span class='top_button chitext font_size_top_button color_top_button'>開始交流</span>
+        <router-link
+          to='/policy'
+          class='text-chinese'
+        >/ 隱私政策</router-link>
       </div>
-      <div class='div_top_svg_1'>
-        <img src='@/assets/svg/pic_landing_page_1.svg'>
+
+      <div><a
+          class='text-chinese'
+          href='http://www.google.com'
+          target='_blank'
+        >/ 聯絡我們</a>
       </div>
+
+      <div>
+        <a
+          class='text-english'
+          href='http://www.google.com'
+          target='_blank'
+        >jobchain2019@gmail.com</a>
+      </div>
+
+      <img src='@/assets/svg/btn_footer_icon_mail_normal.svg' />
+      <div>
+        <a
+          class='text-chinese'
+          href='https://www.facebook.com/groups/jobchain/?ref=bookmarks'
+          target='_blank'
+        >/ 加入<span class='text-english'>VIP</span>社團</a>
+      </div>
+
+      <div>
+        <a
+          class='text-chinese'
+          href='https://www.facebook.com/groups/jobchain/?ref=bookmarks'
+          target='_blank'
+        ><span class='text-english'>Jobchain</span>跨業人才交流平台 / </a>
+      </div>
+
+      <img src='@/assets/svg/bnt_footer_icon_fbgroup_normal.svg' />
+
     </div>
-    <div>
-      <div class='div_top_svg_2'>
-        <img src='@/assets/svg/pic_landing_page_2.svg'>
-      </div>
-    </div>
-    <div>
-      <div class='div_top_svg_3'>
-        <img src='@/assets/svg/pic_landing_page_3.svg'>
-      </div>
-    </div>
-    <div>
-      <div class='div_top_svg_4'>
-        <img src='@/assets/svg/pic_landing_page_4.svg'>
-      </div>
-    </div>
+
   </div>
 </template>
 
-<script>
+<style scoped src='./LandingPage.css'>
+</style>
 
-@import './LandingPage.css'
+<script>
+/* eslint-disable */
 
 export default {
-  name: 'LandingPage',
-  data () {
+  name: "LandingPage",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
---
+++ JobChain/src/components/Policy.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id='policy'>
+    <div class='header'>
+      <h1 class='headtitle'>&nbsp;Jobchain 跨業人才交流計畫</h1>
+    </div>
+    <div class='contents_'>
+      <div
+        class='contents_block'
+        style='width:1726px;height:1566px;margin-top: 91px;margin-left: 97px;'
+      >
+        <p class='policy_content'>
+          隱私政策<br><br>
+          <br>
+          非常歡迎您光臨「Jobchain網站」（以下簡稱本網站），為了讓您能夠安心使用本網站的各項服務與資訊，特此向您說明本網站的隱私權保護政策，以保障您的權益，請您詳閱下列內容：<br>
+          <br><br>
+          一、隱私權保護政策的適用範圍<br><br>
+          　　隱私權保護政策內容，包括本網站如何處理在您使用網站服務時收集到的個人識別資料。隱私權保護政策不適用於本網站以外的相關連結網站，也不適用於非本網站所委託或參與管理的人員。<br>
+          <br><br>
+          二、個人資料的蒐集、處理及利用方式<br><br>
+          　　 • 當您造訪本網站或使用本網站所提供之功能服務時，我們將視該服務功能性質，請您提供必要的個人資料，並在該特定目的範圍內處理及利用您的個人資料；非經您書面同意，本網站不會將<br>
+          　　　個人資料用於其他用途。<br><br>
+          　　 • 本網站在您使用服務信箱、問卷調查等互動性功能時，會保留您所提供的姓名、電子郵件地址、聯絡方式及使用時間等。<br><br>
+          　　 • 於一般瀏覽時，伺服器會自行記錄相關行徑，包括您使用連線設備的IP位址、使用時間、使用的瀏覽器、瀏覽及點選資料記錄等，做為我們增進網站服務的參考依據，此記錄為內部應用，決<br>
+          　　　不對外公佈。<br><br>
+          　　 • 為提供精確的服務，我們會將收集的問卷調查內容進行統計與分析，分析結果之統計數據或說明文字呈現，除供內部研究外，我們會視需要公佈統計數據及說明文字，但不涉及特定個人之資<br>
+          　　　料。<br><br>
+          <br>
+          三、資料之保護<br><br>
+          　　 • 本網站主機均設有防火牆、防毒系統等相關的各項資訊安全設備及必要的安全防護措施，加以保護網站及您的個人資料採用嚴格的保護措施，只由經過授權的人員才能接觸您的個人資料，相<br>
+          　　　關處理人員皆簽有保密合約，如有違反保密義務者，將會受到相關的法律處分。<br><br>
+          　　 • 如因業務需要有必要委託其他單位提供服務時，本網站亦會嚴格要求其遵守保密義務，並且採取必要檢查程序以確定其將確實遵守。<br>
+          　<br><br>
+          四、網站對外的相關連結<br><br>
+          　　本網站的網頁提供其他網站的網路連結，您也可經由本網站所提供的連結，點選進入其他網站。但該連結網站不適用本網站的隱私權保護政策，您必須參考該連結網站中的隱私權保護政策。<br>
+          <br><br>
+          五、與第三人共用個人資料之政策<br><br>
+          　　本網站絕不會提供、交換、出租或出售任何您的個人資料給其他個人、團體、私人企業或公務機關，但有法律依據或合約義務者，不在此限。<br><br>
+          　　前項但書之情形包括不限於：<br><br>
+          　　 • 經由您書面同意。<br><br>
+          　　 • 法律明文規定。<br><br>
+          　　 • 為免除您生命、身體、自由或財產上之危險。<br><br>
+          　　 • 與公務機關或學術研究機構合作，基於公共利益為統計或學術研究而有必要，且資料經過提供者處理或蒐集著依其揭露方式無從識別特定之當事人。<br><br>
+          　　 • 當您在網站的行為，違反服務條款或可能損害或妨礙網站與其他使用者權益或導致任何人遭受損害時，經網站管理單位研析揭露您的個人資料是為了辨識、聯絡或採取法律行動所必要者。<br><br>
+          　　 • 有利於您的權益。<br><br>
+          　　• 本網站委託廠商協助蒐集、處理或利用您的個人資料時，將對委外廠商或個人善盡監督管理之責。<br>
+          <br><br>
+          六、Cookie之使用<br><br>
+          　　為了提供您最佳的服務，本網站會在您的電腦中放置並取用我們的Cookie，若您不願接受Cookie的寫入，您可在您使用的瀏覽器功能項中設定隱私權等級為高，即可拒絕Cookie的寫入，但可<br>
+          　　能會導至網站某些功能無法正常執行 。<br>
+          <br>
+          <br>
+          七、隱私權保護政策之修正<br><br>
+          　　本網站隱私權保護政策將因應需求隨時進行修正，修正後的條款將刊登於網站上。<br><br>
+        </p>
+
+      </div>
+      <router-link
+        to='/'
+        class='start_button'
+        style='margin-top:86.6px;width: 142.6px; height: 65.4px; border-radius: 43px;'
+      >
+        <h1 class='start_button_font'>返回</h1>
+      </router-link>
+    </div>
+    <footer style='margin-top:87px;'>
+      <div class='foot'>
+        <router-link
+          to='/policy'
+          class='foot_policy'
+        >/ 隱私政策</router-link>
+        <a
+          href='https://www.facebook.com/groups/jobchain/?ref=bookmarks'
+          class='foot_invite'
+          target='_blank'
+        >/ 加入VIP社團</a>
+      </div>
+      <div class='foot'>
+        <router-link
+          to='/policy'
+          href='policy.html'
+          class='foot_policy'
+        >/ 關於我們</router-link>
+      </div>
+      <div class='foot'>
+        <a
+          href='policy.html'
+          class='foot_mail'
+        >jobchain2019@gmail.com <img
+            src='@/assets/svg/btn_footer_icon_mail_normal.svg'
+            alt='mail'
+            class='footmailimg'
+          ></a>
+        <a
+          href='https://www.facebook.com/groups/jobchain/?ref=bookmarks'
+          class='foot_fb'
+          target='_blank'
+        >Jobchain跨業人才交流平台 /<img
+            src='@/assets/svg/bnt_footer_icon_fbgroup_normal.svg'
+            alt='facebook'
+            class='footfbimg'
+          /></a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped src='./Policy.css'>
+</style>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: "Policy",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
