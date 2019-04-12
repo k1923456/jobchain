729db38336e62a2132e94e1e7d57ad433266afc2
santai <you@example.com>
2019-04-12 11:48:38 +0800

    Add fonts

---
+++ JobChain/src/components/LandingPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class='navbar'>
+      <span class='engtext font_size_nav'>Jobchain </span><span class='chitext font_size_nav'>跨界人才交流計畫</span>
+    </div>
+    <div>
+      <div class='margin_top_word1'>
+        <span class='engtext font_size_top1'>建立我的專業人脈網</span>
+      </div>
+      <div class='margin_top_word2'>
+        <span class='chitext font_size_nav'>發揮所長，讓專業</span><span class='engtext font_size_nav'>Chain</span><span class='chitext font_size_nav'>在一起</span>
+      </div>
+      <div>
+        <span class='top_button chitext font_size_top_button color_top_button'>開始交流</span>
+      </div>
+      <div class='div_top_svg_1'>
+        <img src='@/assets/svg/pic_landing_page_1.svg'>
+      </div>
+    </div>
+    <div>
+      <div class='div_top_svg_2'>
+        <img src='@/assets/svg/pic_landing_page_2.svg'>
+      </div>
+    </div>
+    <div>
+      <div class='div_top_svg_3'>
+        <img src='@/assets/svg/pic_landing_page_3.svg'>
+      </div>
+    </div>
+    <div>
+      <div class='div_top_svg_4'>
+        <img src='@/assets/svg/pic_landing_page_4.svg'>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+@import './LandingPage.css'
+
+export default {
+  name: 'LandingPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
